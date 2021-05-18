01d17d86871c26fca97500c768408910659f79f6
andmor- <andre@clarin.eu>
2021-05-18 19:06:54 +0000

    Updated from https://github.com/clarin-eric/clarin-drupal-bootstrap-theme/commit/ Last commit message:

---
+++ js/toc.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Initialize TOC
+ *
+ */
+(function($, Drupal) {
+  Drupal.behaviors.clarin = {
+    attach: function attach(context) {
+      if (context === document) {
+        $(function initToc() {
+          var navSelector = "#toc";
+          var scopeSelector = "#tocscope";
+          Toc.init({
+            $nav: $(navSelector, context),
+            $scope: $(scopeSelector, context)
+          });
+          $("body").scrollspy({
+            target: navSelector
+          });
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
