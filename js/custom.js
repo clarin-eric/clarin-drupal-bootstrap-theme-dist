4f91da58e14c2c92198b27a67e03067954c91144
andmor- <andre@clarin.eu>
2024-01-30 21:44:04 +0000

    Updated from https://github.com/clarin-eric/clarin-drupal-bootstrap-theme/commit/55e4bec Last commit message: Some styles for the K Centre search page

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(($, Drupal) => {
+  Drupal.behaviors.clarin_theme_custom = {
+    attach: function attach(context) {
+      if (context !== document || document.readyState !== "interactive") {
+        return;
+      }
+
+      // Form field label as placeholder
+      $(
+        ".mailchimp-signup-subscribe-form .form-type-email, .path-search .search-form .form-type-search",
+        document
+      ).each(() => {
+        const label = $(this)
+          .find("label")
+          .text();
+        $(this)
+          .find(".form-control")
+          .attr("placeholder", label);
+      });
+
+      // Header scroll down arrow function
+      const scrollDownBtn = $(
+        ".paragraph--type--header-large .arrow-down",
+        document
+      );
+      if (scrollDownBtn.length > 0) {
+        scrollDownBtn.click(() => {
+          // scroll to the first paragraph after header image
+          $(".paragraph", context)[1].scrollIntoView();
+        });
+      }
+      // Hide tags if there are more than 2
+      $(
+        ".node--view-mode-teaser-list .field--name-field-tags, .node--view-mode-teaser-big .field--name-field-tags",
+        document
+      ).each(() => {
+        if ($(this).find("li").length > 2) {
+          $(this)
+            .find("ul")
+            .addClass("hide-more");
+          $(this)
+            .find("ul")
+            .append('<li class="show-tags">...</li>');
+        }
+      });
+
+      // Show tags when clicked on show tags button
+      $(".show-tags", document).click(() => {
+        $(this)
+          .parent()
+          .removeClass("hide-more");
+      });
+
+      // Auto select input search box
+      $("#search-menu-dropdown", document).on("shown.bs.dropdown", () => {
+        $("#edit-keys-dropdown", document)
+          .focus()
+          .select();
+      });
+    }
+  };
+})(jQuery, Drupal);
